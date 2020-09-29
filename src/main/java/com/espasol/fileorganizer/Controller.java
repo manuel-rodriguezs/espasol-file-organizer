de99ce01f7bbcf67e6970235269868e8bd5452e3
Manuel Rodríguez <manuel.rodriguezs@gmail.com>
2020-09-29 11:53:44 +0200

    Avanzando en el diseño

--- src/main/java/com/espasol/fileorganizer/Controller.java
+++ src/main/java/com/espasol/fileorganizer/Controller.java
@@ -1,4 +1,94 @@
 package com.espasol.fileorganizer;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+
 public class Controller {
+    @FXML
+    TextField filterField, originField, destField;
+
+    @FXML
+    Button btnOrigen, btnDestino, btnBuscar;
+
+    List<Control> controlsForDisable;
+
+    @FXML
+    protected void selectOriginPath(ActionEvent event) {
+        setDirectoryText(getDirectory(event), originField);
+    }
+
+    @FXML
+    protected void selectDestPath(ActionEvent event) {
+        setDirectoryText(getDirectory(event), destField);
+    }
+
+    @FXML
+    protected void find() {
+        disableFrom();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> enableFrom()));
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
+
+    private Optional<File> getDirectory(ActionEvent event) {
+        Window window = getWindowFromEvent(event);
+        DirectoryChooser chooser = new DirectoryChooser();
+        File file = chooser.showDialog(window);
+        if (file == null) {
+            return Optional.empty();
+        } else {
+            return Optional.of(file);
+        }
+    }
+
+    private Window getWindowFromEvent(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        return source.getScene().getWindow();
+    }
+
+    private void setDirectoryText(Optional<File> directory, TextField field) {
+        if (directory.isPresent()) {
+            field.setText(directory.get().toString());
+        } else {
+            field.setText("");
+        }
+    }
+
+    private void disableFrom() {
+        inicializeControlsForDisable();
+        controlsForDisable.forEach(this::disableControl);
+    }
+
+    private void enableFrom() {
+        inicializeControlsForDisable();
+        controlsForDisable.forEach(this::enableControl);
+    }
+
+    private void inicializeControlsForDisable() {
+        controlsForDisable = asList(filterField, originField, destField, btnOrigen, btnDestino, btnBuscar);
+    }
+
+    private void disableControl(Control c) {
+        c.setDisable(true);
+    }
+
+    private void enableControl(Control c) {
+        c.setDisable(false);
+    }
 }
--- src/main/java/com/espasol/fileorganizer/Main.java
+++ src/main/java/com/espasol/fileorganizer/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
     public static void main(String[] args) {
